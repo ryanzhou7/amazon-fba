1df43349f608a3a2c30517264d7fbd8a3ee72b9e
Ryan <ryanzhou7@gmail.com>
2022-01-15 09:32:15 -0800

    Add dropzone

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+import './App.css';
+import { MyDropzone } from '../file';
+function App() {
+  return (
+    <Box>
+      <MyDropzone />
+    </Box>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 import '@fontsource/roboto/300.css';
