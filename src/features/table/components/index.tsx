e37dc587bbb3800f1e864bbbada10525a67819e3
Ryan <ryanzhou7@gmail.com>
2022-01-16 17:08:11 -0800

    Master detail

--- src/features/table/components/index.tsx
+++ src/features/table/components/index.tsx
@@ -4,7 +4,10 @@ import { AgGridReactProps } from 'ag-grid-react/lib/shared/interfaces';
 import XLSX from 'xlsx';
 
 import './app.scss';
-import { headers } from '../data';
+import { headers, data as mdData } from '../data';
+
+// eslint-disable-next-line unused-imports/no-unused-imports
+import 'ag-grid-enterprise';
 
 const getCompose = (rowData: any) => {
   return rowData.map((r: any) => {
@@ -12,10 +15,12 @@ const getCompose = (rowData: any) => {
     obj[headers[0]] = r[0];
     obj[headers[1]] = r[1];
     obj[headers[2]] = r[2];
-    obj[headers[3]] = r[2];
-    // obj[headers[4]] = r[4];
-    // obj[headers[5]] = r[5];
-    // obj[headers[6]] = r[6];
+    obj[headers[3]] = r[3];
+    obj[headers[4]] = r[4];
+    obj[headers[5]] = r[5];
+    obj[headers[6]] = r[6];
+    obj[headers[7]] = r[7];
+    obj[headers[8]] = r[8];
     return obj;
   });
 };
@@ -48,10 +53,41 @@ export const Table = ({ data, ...rest }: any) => {
           Download
         </Button>
         <div className="ag-theme-material" style={{ height: 500, width: '100%' }}>
-          <AgGridReact defaultColDef={defaultColDef} rowData={rows} {...gridProps}>
-            {headers.map((h: string) => {
+          <AgGridReact
+            // defaultColDef={defaultColDef}
+            rowData={mdData}
+            {...gridProps}
+            masterDetail={true}
+            detailCellRendererParams={{
+              detailGridOptions: {
+                columnDefs: [
+                  { field: 'callId' },
+                  { field: 'direction' },
+                  {
+                    field: 'number',
+                    minWidth: 150,
+                  },
+                  {
+                    field: 'duration',
+                    valueFormatter: "x.toLocaleString() + 's'",
+                  },
+                  {
+                    field: 'switchCode',
+                    minWidth: 150,
+                  },
+                ],
+                defaultColDef: { flex: 1 },
+              },
+              getDetailRowData: function (params: any) {
+                params.successCallback(mdData[0].callRecords);
+              },
+            }}
+          >
+            {/* {headers.map((h: string) => {
               return <AgGridColumn key={h} field={h}></AgGridColumn>;
-            })}
+            })} */}
+            <AgGridColumn field="name" cellRenderer="agGroupCellRenderer" />
+            <AgGridColumn field="account" />
           </AgGridReact>
         </div>
       </Box>
